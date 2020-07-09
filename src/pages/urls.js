9b949bc66cac43d7fc6db05240b441b6ea41ff0c
PolinaVdovina <polina.vdovina.98@inbox.ru>
2020-07-09 17:09:01 +0700

    urls

---
+++ src/pages/urls.js
@@ -0,0 +1,17 @@
+export const urls = {
+    registration: {
+        title: 'Регистрация',
+        path: '/register/',
+        shortPath: '/register/',
+    },
+    autentification: {
+        title: 'Аутентификация',
+        path: '/auth/',
+        shortPath: '/auth/',
+    },
+    cabinet: {
+        title: 'Кабинет',
+        path: '/cabinet/:id',
+        shortPath: '/cabinet/',
+    }
+}
